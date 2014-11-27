e5b8923f5671724d22f6d08d059186f66d12345a
andresn <andresn@yahoo-inc.com>
2014-11-26 19:44:41 -0800

    a lil cleanup and adding some translated comments

--- MuseScore/plugins/findharmonies/findharmonies.js
+++ MuseScore/plugins/findharmonies/findharmonies.js
@@ -9,10 +9,9 @@
 //    this function will be called on startup of mscore
 //---------------------------------------------------------
 
-function init()
-      {
-      // print("Find harmonies plugin");
-      }
+function init() {
+  // print("Find harmonies plugin");
+}
 
 //-------------------------------------------------------------------
 //    run
@@ -37,37 +36,38 @@ function run(){
           var grundton=chord.note(0).pitch;
           var diff1=chord.note(1).pitch-grundton;
           var diff2=chord.note(2).pitch-grundton;
-          if ((diff1==4)&&(diff2==7)){ //Dur-Akkord
+          if ((diff1==4)&&(diff2==7)){ //Dur-Akkord // en-US: n/a (guess: stop chord?)
             ++erkannt;
             var text  = new Text(curScore);
             text.text = tone(grundton%12);
             cursor.putStaffText(text);
           }
-          if ((diff1==5)&&(diff2==9)){ //Dur-Akkord 1. Umkehrung
+          if ((diff1==5)&&(diff2==9)){ //Dur-Akkord 1. Umkehrung // en-US: reversal
             ++erkannt;
             var text  = new Text(curScore);
             text.text = tone((grundton+5)%12)+"¹";
             cursor.putStaffText(text);
           }
-          if ((diff1==3)&&(diff2==8)){ //Dur-Akkord 2. Umkehrung
+          if ((diff1==3)&&(diff2==8)){ //Dur-Akkord 2. Umkehrung // en-US: 2. reversal
             ++erkannt;
             var text  = new Text(curScore);
             text.text = tone((grundton+8)%12)+"²";
             cursor.putStaffText(text);
           }
-          if ((diff1==3)&&(diff2==7)){ //Moll-Akkord
+          if ((diff1==3)&&(diff2==7)){ //Moll-Akkord // en-US: minor chord
             ++erkannt;
             var text  = new Text(curScore);
             text.text = tone(grundton%12)+"m";
             cursor.putStaffText(text);
           }
-          if ((diff1==5)&&(diff2==8)){ //Moll-Akkord 1. Umkehrung
+          if ((diff1==5)&&(diff2==8)){ //Moll-Akkord 1. Umkehrung // en-US: minor chord 1. reversal
             ++erkannt;
             var text  = new Text(curScore);
             text.text = tone((grundton+5)%12)+"m¹";
             cursor.putStaffText(text);
           }
-          if ((diff1==4)&&(diff2==9)){ //Moll-Akkord 2. Umkehrung
+          if ((diff1==4)&&(diff2==9)){ //Moll-Akkord 2. Umkehrung 
+                                       // en-US: minor chord 1. reversal
             ++erkannt;
             var text  = new Text(curScore);
             text.text = tone((grundton+9)%12)+"m²";
@@ -80,24 +80,28 @@ function run(){
           var diff2=chord.note(2).pitch-grundton;
           var diff3=chord.note(3).pitch-grundton;
           if ((diff1==4)&&(diff2==7)&&(diff3==10)){ //Septime-Akkord
+                                                    // en-US: n/a (guess: 7th chord?)
             ++erkannt;
             var text  = new Text(curScore);
             text.text = tone(grundton%12)+"7";
             cursor.putStaffText(text);
           }
           if ((diff1==2)&&(diff2==6)&&(diff3==9)){ //Septime-Akkord 1. Umkehrung
+                                                   // en-US: n/a (guess: 7th chord?) 1. reversal
             ++erkannt;
             var text  = new Text(curScore);
             text.text = tone((grundton+2)%12)+"7¹";
             cursor.putStaffText(text);
           }
           if ((diff1==3)&&(diff2==5)&&(diff3==9)){ //Septime-Akkord 2. Umkehrung
+                                                   // en-US: n/a (guess: 7th chord?) 1. reversal
             ++erkannt;
             var text  = new Text(curScore);
             text.text = tone((grundton+5)%12)+"7²";
             cursor.putStaffText(text);
           }
           if ((diff1==3)&&(diff2==6)&&(diff3==8)){ //Septime-Akkord 3. Umkehrung
+                                                  // en-US: n/a (guess: 7th chord?) 1. reversal
             ++erkannt;
             var text  = new Text(curScore);
             text.text = tone((grundton+8)%12)+"7³";
@@ -164,9 +168,9 @@ function tone(halbton) {
 //---------------------------------------------------------
 
 var mscorePlugin = {
-      menu: 'Plugins.Find harmonies',
-      init: init,
-      run:  run
-      };
+  menu: 'Plugins.Find harmonies',
+  init: init,
+  run:  run
+};
 
 mscorePlugin;
